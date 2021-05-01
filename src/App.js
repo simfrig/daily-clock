a6e48b1d7408278805e3bfa81a8ce72cd7328585
simfrig <74631160+simfrig@users.noreply.github.com>
2021-05-01 17:11:43 -0400

    Update App.js
    Code Working - Only Need to make Time Update on itself.

--- src/App.js
+++ src/App.js
@@ -6,32 +6,44 @@ import React, { useState } from "react";
 
 function App() { 
 
+  setInterval(myGreetings);
+  
+ 
   
   
   const [greetings, setGreetings] = useState('');
-  let date = new Date();
-  let specificTime = date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false})
+  const [time, setTime] = useState('');
+  
 
+  let date = new Date();
   let hour = date.getHours();
 
- function myGreetings() {
-    if (hour < 12) {
+  function currentTime() {
+  setTime(date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false}));
+  }
+
+
+
+   setInterval(currentTime);
+
+    function myGreetings() {
+      if (hour < 12) {
       setGreetings("Good Morning Toronto!");
     } else if (hour > 12) {
       setGreetings("Good Afternoon Toronto!");
     }
   }
 
-  
-  
   return ( 
 <div className = "App" >
     <div className = "time">
-    <h1 className = "title">{specificTime}</h1>
+    <h1 className = "title">{time}</h1>
     <span>{greetings}</span> 
     </div>
 </div>
   );
+
+  
 }
 
 export default App;
