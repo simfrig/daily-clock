fed9ca538a5235562f526847466fcf65aee658ce
simfrig <74631160+simfrig@users.noreply.github.com>
2021-05-03 03:38:02 -0400

    Update Time
    Time now update automatically

--- src/App.js
+++ src/App.js
@@ -7,24 +7,19 @@ import React, { useState } from "react";
 function App() { 
 
   setInterval(myGreetings);
-  
- 
-  
-  
+    
   const [greetings, setGreetings] = useState('');
   const [time, setTime] = useState('');
   
-
   let date = new Date();
   let hour = date.getHours();
 
   function currentTime() {
+  let date = new Date();
   setTime(date.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false}));
   }
 
-
-
-   setInterval(currentTime);
+   setInterval(currentTime,1000);
 
     function myGreetings() {
       if (hour < 12) {
@@ -40,6 +35,10 @@ function App() {
     <h1 className = "title">{time}</h1>
     <span>{greetings}</span> 
     </div>
+
+<div className="weatherWidget">
+
+</div>
 </div>
   );
 
